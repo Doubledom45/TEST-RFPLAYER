bedbce551e53c38ec8427bdef3fd2c79b06a4af0
Doubledom <97252459+Doubledom45@users.noreply.github.com>
2024-03-10 12:18:32 +0000

    Update config_flow.py

--- custom_components/rfplayer/config_flow.py
+++ custom_components/rfplayer/config_flow.py
@@ -49,7 +49,7 @@ class RfplayerConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         data = {
             vol.Required(CONF_DEVICE): vol.In(list_of_ports),
             vol.Required(CONF_AUTOMATIC_ADD, default=True): bool,
-            vol.Required(CONF_FORMAT, default=""): str,
+            vol.Required(CONF_FORMAT, default="JSON"): str,
             vol.Required(
                 CONF_RECONNECT_INTERVAL, default=DEFAULT_RECONNECT_INTERVAL
             ): int,
@@ -78,30 +78,148 @@ class RfPlayerOptionsFlowHandler(config_entries.OptionsFlow):
         """Initialize."""
         self.config_entry = config_entry
 
+    # async def async_step_init(self, user_input: dict = None):
+    #     """Manage the options."""
+    #     log.debug("Init:%s",user_input)
+ 
+    #     data = self.config_entry.data.copy()
+    #     data[CONF_AUTOMATIC_ADD] = True
+    #     data[CONF_FORMAT] = "JSON"
+    #     data[CONF_FREQ_H] = 868350
+    #     data[CONF_FREQ_L] = 433920
+    #     data[CONF_SELECTIVITY_H] = 0
+    #     data[CONF_SELECTIVITY_L] = 0
+    #     data[CONF_SENSITIVITY_H] = 4
+    #     data[CONF_SENSITIVITY_L] = 4
+    #     data[CONF_DSPTRIGGER_H] = 6
+    #     data[CONF_DSPTRIGGER_L] = 8
+    #     data[CONF_RFLINK] = 1
+    #     data[CONF_RFLINKTRIGGER_H] = 10
+    #     data[CONF_RFLINKTRIGGER_L] = 12
+    #     data[CONF_LBT] = 16
+    #     data[CONF_LEDACTIVITY] = 1
+    #     data[CONF_RECEIVER_DISABLE] = ""
+    #     data[CONF_REPEATER_DISABLE] = ""
+    #     data[CONF_TRACE] = ""
+    #     return self.async_create_entry(title=data[CONF_DEVICE], data=data)
+
     async def async_step_init(self, user_input: dict = None):
         """Manage the options."""
         log.debug("Init:%s",user_input)
- 
-        data = self.config_entry.data.copy()
-        data[CONF_AUTOMATIC_ADD] = True
-        data[CONF_FORMAT] = "JSON"
-        data[CONF_FREQ_H] = 868350
-        data[CONF_FREQ_L] = 433920
-        data[CONF_SELECTIVITY_H] = 0
-        data[CONF_SELECTIVITY_L] = 0
-        data[CONF_SENSITIVITY_H] = 4
-        data[CONF_SENSITIVITY_L] = 4
-        data[CONF_DSPTRIGGER_H] = 6
-        data[CONF_DSPTRIGGER_L] = 8
-        data[CONF_RFLINK] = 1
-        data[CONF_RFLINKTRIGGER_H] = 10
-        data[CONF_RFLINKTRIGGER_L] = 12
-        data[CONF_LBT] = 16
-        data[CONF_LEDACTIVITY] = 1
-        data[CONF_RECEIVER_DISABLE] = ""
-        data[CONF_REPEATER_DISABLE] = ""
-        data[CONF_TRACE] = ""
-        return self.async_create_entry(title=data[CONF_DEVICE], data=data)
+        if user_input is None:
+            config = self.config_entry.data
+            options = self.config_entry.options
+            Init_Schema={
+                        
+                        vol.Required(
+                            CONF_AUTOMATIC_ADD,
+                            default=options.get(CONF_AUTOMATIC_ADD, config.get(CONF_AUTOMATIC_ADD,True))
+                        ): bool,
+                        vol.Required(
+                            CONF_FORMAT, 
+                            default=options.get(CONF_FORMAT, config.get(CONF_FORMAT,"JSON"))
+                        ): vol.In(["OFF", "BINARY", "HEXA", "HEXA_FIXED","TEXT","XML","JSON"]),
+                        vol.Required(
+                            CONF_FREQ_H, 
+                            default=options.get(CONF_FREQ_H, config.get(CONF_FREQ_H,868350))
+                        ): vol.In(["0", "868950", "868350"]),
+                        vol.Required(
+                            CONF_FREQ_L, 
+                            default=options.get(CONF_FREQ_L, config.get(CONF_FREQ_L,433920))
+                        ): vol.In(["0", "433420", "433920"]),
+                        vol.Required(
+                            CONF_SELECTIVITY_H, 
+                            default=options.get(CONF_SELECTIVITY_H, config.get(CONF_SELECTIVITY_H,0))
+                        ): vol.In(["0", "1", "2", "3", "4", "5"]),
+                        vol.Required(
+                            CONF_SELECTIVITY_L, 
+                            default=options.get(CONF_SELECTIVITY_L, config.get(CONF_SELECTIVITY_L,0))
+                        ): vol.In(["0", "1", "2", "3", "4", "5"]),
+                        vol.Required(
+                            CONF_SENSITIVITY_H, 
+                            default=options.get(CONF_SENSITIVITY_H, config.get(CONF_SENSITIVITY_H,4))
+                        ): vol.In(["0", "1", "2", "3", "4", "5"]),
+                        vol.Required(
+                            CONF_SENSITIVITY_L, 
+                            default=options.get(CONF_SENSITIVITY_L, config.get(CONF_SENSITIVITY_L,4))
+                        ): vol.In(["0", "1", "2", "3", "4", "5"]),
+                        vol.Required(
+                            CONF_DSPTRIGGER_H, 
+                            default=options.get(CONF_DSPTRIGGER_H, config.get(CONF_DSPTRIGGER_H,6))
+                        ): int,
+                        vol.Required(
+                            CONF_DSPTRIGGER_L, 
+                            default=options.get(CONF_DSPTRIGGER_L, config.get(CONF_DSPTRIGGER_L,8))
+                        ): int,
+                        vol.Required(
+                            CONF_RFLINK, 
+                            default=options.get(CONF_RFLINK, config.get(CONF_RFLINK,True))
+                        ): bool,
+                        vol.Required(
+                            CONF_RFLINKTRIGGER_H, 
+                            default=options.get(CONF_RFLINKTRIGGER_H, config.get(CONF_RFLINKTRIGGER_H,10))
+                        ): int,
+                        vol.Required(
+                            CONF_RFLINKTRIGGER_L, 
+                            default=options.get(CONF_RFLINKTRIGGER_L, config.get(CONF_RFLINKTRIGGER_L,12))
+                        ): int,
+                        vol.Required(
+                            CONF_LBT, 
+                            default=options.get(CONF_LBT, config.get(CONF_LBT,16))
+                        ): int,
+                        vol.Required(
+                            CONF_LEDACTIVITY, 
+                            default=options.get(CONF_LEDACTIVITY, config.get(CONF_LEDACTIVITY,True))
+                        ): bool,
+                        
+                    }
+
+            Init_Schema[CONF_RECEIVER_DISABLE] = selector({
+                "select": {
+                    "options": ["X10", "RTS", "VISONIC", "BLYSS", "CHACON", "OREGONV1", "OREGONV2", "OREGONV3/OWL", "DOMIA", "X2D", "KD101", "PARROT", "TIC", "FS20", "JAMMING", "EDISIO"],
+                    "multiple":True
+                },
+            })
+            Init_Schema[CONF_REPEATER_DISABLE] = selector({
+                "select": {
+                    "options": ["X10", "RTS", "VISONIC", "BLYSS", "CHACON", "OREGONV1", "OREGONV2", "OREGONV3/OWL", "DOMIA", "X2D", "KD101", "PARROT", "TIC", "FS20", "JAMMING", "EDISIO"],
+                    "multiple":True
+                },
+            })
+            Init_Schema[CONF_TRACE] = selector({
+                "select": {
+                    "options": ["ALARM", "RECEIVER", "TRANSMITTER", "TRANSCODER", "REPEATER", "JAMMING", "RFLINK"],
+                    "multiple":True
+                },
+            })
+
+            return self.async_show_form(
+                step_id="init",
+                data_schema=vol.Schema(Init_Schema),
+            )
+
+        else:
+            data = self.config_entry.data.copy()
+            data[CONF_AUTOMATIC_ADD] =True
+            data[CONF_FORMAT] = "JSON"
+            data[CONF_FREQ_H] = 868350
+            data[CONF_FREQ_L] = 433920
+            data[CONF_SELECTIVITY_H] = 0
+            data[CONF_SELECTIVITY_L] = 0
+            data[CONF_SENSITIVITY_H] = 4
+            data[CONF_SENSITIVITY_L] = 4
+            data[CONF_DSPTRIGGER_H] = 6
+            data[CONF_DSPTRIGGER_L] = 8
+            data[CONF_RFLINK] = True
+            data[CONF_RFLINKTRIGGER_H] = 10
+            data[CONF_RFLINKTRIGGER_L] = 12
+            data[CONF_LBT] = 16
+            data[CONF_LEDACTIVITY] = True
+            data[CONF_RECEIVER_DISABLE] = ""
+            data[CONF_REPEATER_DISABLE] = ""
+            data[CONF_TRACE] = ""
+            return self.async_create_entry(title=data[CONF_DEVICE], data=data)
+
 
 
 class CannotConnect(exceptions.HomeAssistantError):
